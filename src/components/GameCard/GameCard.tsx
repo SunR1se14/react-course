759dfb2d4076bcf828788ad45fd744429a0d5715
Arthur Kosyak <rivey322@gmail.com>
2024-07-07 12:00:28 +0500

    feat: update info game

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -1,19 +1,34 @@
-import { Component, ReactNode } from 'react'
+import { Component } from 'react'
+import cn from 'clsx'
+import { IGame } from '../../types/game.interface'
 import styles from './GameCard.module.scss'
 
-class GameCard extends Component {
-  render(): ReactNode {
+class GameCard extends Component<IGame> {
+  bgRating = () => {
+    const { rating } = this.props
+    if (rating > 4) {
+      return styles.good
+    } else if (rating > 3) {
+      return styles.normal
+    } else {
+      return styles.bad
+    }
+  }
+
+  render() {
+    const { name, background_image, rating, genres } = this.props
+    const genresArr = genres.map(genre => genre.name).join(', ')
+
     return (
       <div className={styles.card}>
         <div className={styles.img}>
-          <img
-            src="https://media.rawg.io/media/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg"
-            alt="Game image"
-          />
+          <img src={background_image} alt="Game image" />
         </div>
-        <h4 className={styles.title}>Grand Theft Auto V</h4>
-        <div className={styles.rating}>4.5 / 5</div>
-        <div className={styles.genre}>Action</div>
+        <h4 className={styles.title}>{name}</h4>
+        <div className={cn(styles.rating, this.bgRating())}>
+          {rating.toFixed(1)} / 5
+        </div>
+        <div className={styles.genre}>{genresArr}</div>
       </div>
     )
   }
