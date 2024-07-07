0dc4b56e62f2e018aad2c1389bc13d88dedad7a0
Arthur Kosyak <rivey322@gmail.com>
2024-07-07 11:43:07 +0500

    feat: add loading

--- src/components/GameList/GameList.tsx
+++ src/components/GameList/GameList.tsx
@@ -1,11 +1,12 @@
 import { Component } from 'react'
 import GameCard from '../GameCard/GameCard'
-import styles from './GameList.module.scss'
 import { IGame } from '../../types/game.interface'
 import { IResponse } from '../../types/response,interface'
+import styles from './GameList.module.scss'
 
 interface State {
   games: IGame[]
+  isLoading: boolean
 }
 
 class GameList extends Component<Record<string, never>, State> {
@@ -13,15 +14,23 @@ class GameList extends Component<Record<string, never>, State> {
     super(props)
     this.state = {
       games: [],
+      isLoading: false,
     }
   }
 
   async getGamesData() {
-    const res = await fetch(
-      'https://api.rawg.io/api/games?key=a2a625bfa01a46bbb1a130f28fc8ee34&page_size=9&page=1',
-    )
-    const data: IResponse = await res.json()
-    this.setState({ games: data.results })
+    try {
+      this.setState({ isLoading: true })
+      const res = await fetch(
+        'https://api.rawg.io/api/games?key=a2a625bfa01a46bbb1a130f28fc8ee34&page_size=9&page=1',
+      )
+      const data: IResponse = await res.json()
+      this.setState({ games: data.results })
+    } catch (error) {
+      console.error(error)
+    } finally {
+      this.setState({ isLoading: false })
+    }
   }
 
   componentDidMount() {
@@ -29,7 +38,11 @@ class GameList extends Component<Record<string, never>, State> {
   }
 
   render() {
-    const { games } = this.state
+    const { games, isLoading } = this.state
+
+    if (isLoading) {
+      return <div className={styles['loading']}>Loading...</div>
+    }
 
     return (
       <div className={styles['card-list']}>
